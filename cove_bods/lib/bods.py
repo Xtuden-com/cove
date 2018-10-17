5f13a8f9016092936863fb8ea67f62c74fd0d006
rhiaro <amy@rhiaro.co.uk>
2018-10-17 17:48:52 +0300

    Init BODS

--- cove_bods/lib/bods.py
+++ cove_bods/lib/bods.py
@@ -6,4 +6,4 @@ import cove.lib.tools as tools
 from cove.lib.common import common_checks_context, get_orgids_prefixes
 
 def common_checks_bods(context, upload_dir, json_data, schema_obj):
-    pass
+    return context
--- cove_bods/lib/schema.py
+++ cove_bods/lib/schema.py
@@ -14,3 +14,24 @@ class SchemaBODS(SchemaJsonMixin):
     release_pkg_schema_name = config['schema_name']
     release_schema_url = urljoin(schema_host, release_schema_name)
     release_pkg_schema_url = urljoin(schema_host, release_pkg_schema_name)
+
+    def __init__():
+        pass
+
+    def process_codelists():
+        pass
+
+    def get_entity_schema_obj():
+        pass
+
+    def get_person_schema_obj():
+        pass
+
+    def get_ownership_schema_obj():
+        pass
+
+    def get_components_schema_obj():
+        pass
+
+    def get_package_schema_obj():
+        pass
